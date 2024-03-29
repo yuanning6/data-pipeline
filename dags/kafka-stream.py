2b32ed07ad60efb797dc87c5ba87d1b7babe86dd
evelynliu <1821584441@qq.com>
2024-03-29 17:47:01 -0500

    Setup spark, cassandra, kafka connection

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -38,22 +38,31 @@ def stream_data():
     import json
     from kafka import KafkaProducer
     import time
+    import logging
 
-    res = get_data()
-    res = format_data(res)
-    # print(json.dumps(res, indent=3))
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    while True:
+        if time.time() > curr_time + 60: # 1 min
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
 
-# with DAG('user_automation',
-#          default_args=default_args,
-#          schedule_interval='@daily',
-#          catchup=False) as dag:
-#
-#     streaming_task = PythonOperator(
-#         task_id='stream_date_from_api',
-#         python_callable=stream_data
-#     )
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
-stream_data()
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_date_from_api',
+        python_callable=stream_data
+    )
+
+# stream_data()
